79bcb36e51f6bdb8b8a7cb7e0635a9493af7eaa9
Alvaro <mapaaas@gmail.com>
2021-10-21 12:57:53 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1> {{ titulo }} </h1>
+      <ul> 
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+     titulo: 'Alurapic',
+     fotos: [
+       {
+        url: 'https://exame.com/wp-content/uploads/2017/05/darpa-boeing.jpg',
+        titulo: 'foguete'
+       },
+       {
+        url: 'https://exame.com/wp-content/uploads/2017/05/darpa-boeing.jpg',
+        titulo: 'foguete2'
+       }
+     ]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
